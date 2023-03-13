4004da2dcc169726d346b0e43ffb4e62a8bef68f
LiebeV <68876097+LiebeV@users.noreply.github.com>
2023-03-13 23:22:35 +0100

    Update **WIP**斗鱼web全等级弹幕屏蔽.user.js

--- **WIP**斗鱼web全等级弹幕屏蔽.user.js
+++ **WIP**斗鱼web全等级弹幕屏蔽.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         高级弹幕屏蔽
 // @namespace    https://www.liebev.site
-// @version      0.2
+// @version      0.2.1
 // @description  douyu斗鱼，高级弹幕屏蔽，突破30级等级屏蔽限制
 // @author       LiebeV
 // @license      MIT; https://opensource.org/licenses/MIT; LiebeV
@@ -14,41 +14,42 @@
 
 'use strict';
 
-
-// async function getList() {
-//     //获取当前列表
-
-//     const dmList = ulList.children;
-
-//     for (var i = 0; i < dmList.length; i++) {
-//         var spanList = dmList[i].querySelectorAll("span.Barrage-nickName[title]");
-//         for (var j = 0; j < spanList.length; j++) {
-//             var title = spanList[j].getAttribute("title");
-//             // 输出标题到控制台
-//             console.log(title);
-//         }
-//     }
-// }
-
-//尝试获取弹幕区
-const ulList = document.getElementById("js-barrage-list");
-console.log('已获取弹幕区');
-console.log(ulList);
 let bannedIds = GM_getValue('bannedIds', '[]');
 
+//尝试获取弹幕区
+async function getBarrageList() {
+    console.log('脚本初始化成功');
 
-//比对id，更改父元素css
+    let ulList;
+    while (!ulList) {
+        ulList = document.getElementById("js-barrage-list");
+
+        console.log('尝试获取弹幕区...');        
+        if (!ulList) {
+            console.log('失败，等待1秒钟再尝试获取');
+            await new Promise(resolve => setTimeout(resolve, 1000));
+        }
+    }
+    console.log('已获取弹幕区');
+    console.log(ulList);
+    console.log('准备开启mutation感知');
+    return ulList;
+}
+
+//比对id，更改父级li元素css
 async function ban(userId) {
     console.log("ban函数接收到userId，进行放行判断");
-    console.log(userId);
+
     if (bannedIds.includes(userId)) {
         console.log("弹幕进入屏蔽分支");
         const spanElements = document.querySelectorAll(`span[title="${userId}"]`);
         console.log("已选取需屏蔽的弹幕元素");
+
         for (let i = spanElements.length - 1; i >= 0; i--) {
             const spanElement = spanElements[i];
             const liElement = spanElement.parentNode.parentNode;
             liElement.style.display = 'none';
+
             console.log(`"${userId}" 的发言已被屏蔽!`);
         }
     }else{
@@ -66,6 +67,9 @@ async function getId(node) {
 
 //mutation observer
 async function MO() {
+    const ulList = await getBarrageList();
+    console.log('感知准备完成');
+
     const observer = new MutationObserver(function (mutations) {
         mutations.forEach(function (mutation) {
             mutation.addedNodes.forEach(function (node) {
@@ -77,14 +81,15 @@ async function MO() {
             });
         });
     });
-
     const config = { childList: true };
     observer.observe(ulList, config);
+    console.log('感知挂载完成');
 }
 
 //通过setValue像存储器添加id
 async function addId(userIdInput) {
     console.log(`addId接收到传入`);
+
     if (!bannedIds.includes(userIdInput)) {
         bannedIds.push(userIdInput);
         console.log(`已添加屏蔽用户： ${userIdInput}`);
@@ -94,6 +99,8 @@ async function addId(userIdInput) {
     }
 }
 
+GM_registerMenuCommand('添加屏蔽用户', showSettings);
+
 async function showSettings() {
     const userIdInput = prompt(`请输入要屏蔽发言的用户id：（当你下次进入房间时生效）`);
     if (userIdInput) {
@@ -102,12 +109,10 @@ async function showSettings() {
     }
 }
 
-GM_registerMenuCommand('添加屏蔽用户', showSettings);
-
 (function () {
     MO();
 })();
 
 //v0.1 -- 依托答辩
-//v0.2 -- 右侧弹幕区屏蔽功能完善
+//v0.2.1 -- 右侧弹幕区屏蔽功能完善
 //（v0.3） -- 飘屏区弹幕屏蔽
